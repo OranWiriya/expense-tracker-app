9e39a3de329e4478c40a34e9014b7a128cb952d5
OranWiriya <megamanoran@gmail.com>
2026-08-30 19:05:04 +0700

    💄 [UI] add ShadUI for be base component for the project, and add example component button for shown the result, change font-style from Geist to IBM-plex-sans both bilingual (Thai, English)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import { Button, buttonVariants } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
@@ -21,26 +22,30 @@ export default function Home() {
             file.
           </h1>
           <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
+            ภาษาไทย
             <a
               href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
+              className={buttonVariants({
+                variant: "link",
+                size: "lg",
+                className: "font-bold text-zinc-900 dark:text-zinc-50",
+              })}
             >
-              Templates
+              เท็มเพลต
             </a>{" "}
             or the{" "}
             <a
               href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
+              className={buttonVariants({ variant: "link", size: "lg" })}
             >
-              Learning
+              เรียนรู้
             </a>{" "}
             center.
           </p>
         </div>
         <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
           <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
+            className={buttonVariants({ variant: "default", size: "lg" })}
             href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
             target="_blank"
             rel="noopener noreferrer"
@@ -55,13 +60,14 @@ export default function Home() {
             Deploy Now
           </a>
           <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
+            className={buttonVariants({ variant: "ghost", size: "lg" })}
             href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
             target="_blank"
             rel="noopener noreferrer"
           >
             Documentation
           </a>
+          <Button>test</Button>
         </div>
       </main>
     </div>
