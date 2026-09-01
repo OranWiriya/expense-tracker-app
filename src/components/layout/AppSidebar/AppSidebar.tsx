e75871455fdd71c6f3348b05b274bec18482da20
OranWiriya <megamanoran@gmail.com>
2026-09-01 16:23:21 +0700

    🚧 [feature] update header icon, titleName, collape button and collaped sidbar sizing

--- src/components/layout/AppSidebar/AppSidebar.tsx
+++ src/components/layout/AppSidebar/AppSidebar.tsx
@@ -2,17 +2,17 @@ import AppSidebarHeader from "./AppSidebarHeader";
 import AppSidebarFooter from "./AppSidebarFooter";
 import AppSidebarContent from "./AppSidebarContent";
 import { Sidebar, SidebarTrigger } from "@/components/ui/sidebar";
+import { Fragment } from "react/jsx-runtime";
 
 const AppSidebar = (props) => {
   const { header, content, footer } = props;
   return (
     <>
-      <Sidebar collapsible="icon" variant="inset">
+      <Sidebar collapsible="icon" variant="inset" className="border-r px-0">
         <AppSidebarHeader header={header} />
         <AppSidebarContent content={content} />
         <AppSidebarFooter footer={footer} />
       </Sidebar>
-      <SidebarTrigger />
     </>
   );
 };
--- src/components/layout/AppSidebar/AppSidebarHeader.tsx
+++ src/components/layout/AppSidebar/AppSidebarHeader.tsx
@@ -1,9 +1,39 @@
-import { SidebarHeader } from "@/components/ui/sidebar";
+"use client";
+
+import {
+  SidebarHeader,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarTrigger,
+  useSidebar,
+} from "@/components/ui/sidebar";
+import { Cat } from "lucide-react";
 
 const AppSidebarHeader = ({ header }: { header: string[] }) => {
+  const { state } = useSidebar();
   return (
-    <SidebarHeader>
-      <h1>App Header</h1>
+    <SidebarHeader className="border-b">
+      <SidebarMenu
+        className={
+          state === "collapsed"
+            ? "flex-row items-center justify-between"
+            : "flex-row items-center"
+        }
+      >
+        <SidebarMenuButton
+          size={"lg"}
+          tooltip={"Expense Tracker"}
+          className="font-bold hover:bg-primary hover:text-primary-foreground"
+        >
+          <div className="flex size-12 items-center justify-center">
+            <Cat className="size-8! " />
+          </div>
+          {state === "expanded" && (
+            <span className="text-sm">EXPENSE TRACKER</span>
+          )}
+        </SidebarMenuButton>
+        <SidebarTrigger />
+      </SidebarMenu>
     </SidebarHeader>
   );
 };
