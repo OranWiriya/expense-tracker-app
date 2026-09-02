9fc3f59560fb468aff4ac829848f4686b2769560
OranWiriya <megamanoran@gmail.com>
2026-09-02 15:35:47 +0700

    💄update style follow the designer intention

--- src/components/layout/AppSidebar/AppSidebar.tsx
+++ src/components/layout/AppSidebar/AppSidebar.tsx
@@ -1,14 +1,13 @@
 import AppSidebarHeader from "./AppSidebarHeader";
 import AppSidebarFooter from "./AppSidebarFooter";
 import AppSidebarContent from "./AppSidebarContent";
-import { Sidebar, SidebarTrigger } from "@/components/ui/sidebar";
-import { Fragment } from "react/jsx-runtime";
+import { Sidebar } from "@/components/ui/sidebar";
 
 const AppSidebar = (props) => {
   const { header, content, footer } = props;
   return (
     <>
-      <Sidebar collapsible="icon" variant="inset" className="border-r px-0">
+      <Sidebar collapsible="icon" variant="inset" className="border-r p-0">
         <AppSidebarHeader header={header} />
         <AppSidebarContent content={content} />
         <AppSidebarFooter footer={footer} />
--- src/components/layout/AppSidebar/AppSidebarHeader.tsx
+++ src/components/layout/AppSidebar/AppSidebarHeader.tsx
@@ -13,26 +13,22 @@ const AppSidebarHeader = ({ header }: { header: string[] }) => {
   const { state } = useSidebar();
   return (
     <SidebarHeader className="border-b">
-      <SidebarMenu
-        className={
-          state === "collapsed"
-            ? "flex-row items-center justify-between"
-            : "flex-row items-center"
-        }
-      >
+      <SidebarMenu className={"flex-row items-center justify-between p-0"}>
         <SidebarMenuButton
           size={"lg"}
           tooltip={"Expense Tracker"}
-          className="font-bold hover:bg-primary hover:text-primary-foreground"
+          className="font-bold p-0 pr-2 justify-between group-data-[collapsible=icon]:size-12!  hover:bg-primary hover:text-primary-foreground "
         >
-          <div className="flex size-12 items-center justify-center">
+          <div className="flex size-8 items-center justify-left group-data-[collapsible=icon]:w-8!">
             <Cat className="size-8! " />
           </div>
           {state === "expanded" && (
             <span className="text-sm">EXPENSE TRACKER</span>
           )}
         </SidebarMenuButton>
-        <SidebarTrigger />
+        <SidebarTrigger
+          className={"size-10 group-data-[collapsible=icon]:size-10!"}
+        />
       </SidebarMenu>
     </SidebarHeader>
   );
