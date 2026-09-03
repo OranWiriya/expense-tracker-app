5ece799b9d7e2e687c381a47fea2f8fc3a63d7ee
OranWiriya <megamanoran@gmail.com>
2026-09-03 16:56:32 +0700

    💄 [feature] implement dynamic sidebar navigation content and Footer

--- src/components/layout/AppSidebar/AppSidebarContent.tsx
+++ src/components/layout/AppSidebar/AppSidebarContent.tsx
@@ -1,7 +1,70 @@
-import { SidebarContent } from "@/components/ui/sidebar";
+"use client";
 
-const AppSidebarContent = ({ content }: { content: string[] }) => {
-  return <SidebarContent>AppSidebarHeader</SidebarContent>;
+import {
+  SidebarContent,
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarGroupLabel,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+  useSidebar,
+} from "@/components/ui/sidebar";
+import { NavContentType } from "./nav-data";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import { isActiveNavMenu } from "./utils";
+
+const AppSidebarContent = ({ content }: { content: NavContentType[] }) => {
+  const pathname = usePathname();
+  const { state } = useSidebar();
+
+  const classNameSidebarButtonBase =
+    " hover:text-primary-foreground data-active:text-primary-foreground data-active:font-bold data-active:hover:text-primary-foreground";
+
+  return (
+    <>
+      <SidebarContent>
+        {content.map((contentItem, index) => (
+          <SidebarGroup key={contentItem.title + index}>
+            <SidebarGroupLabel className="text-base uppercase">
+              {contentItem.title}
+            </SidebarGroupLabel>
+            <SidebarGroupContent>
+              <SidebarMenu>
+                {contentItem.items?.map((item, index) => {
+                  return (
+                    <SidebarMenuItem key={item.title + index}>
+                      <SidebarMenuButton
+                        isActive={isActiveNavMenu({ pathname, url: item.url })}
+                        render={<Link href={item.url} />}
+                        variant={"outline"}
+                        size={"lg"}
+                        className={
+                          (state === "collapsed"
+                            ? "min-w-full justify-center items-center"
+                            : "") + classNameSidebarButtonBase
+                        }
+                      >
+                        {item.icon && <item.icon className={"size-6!"} />}
+                        {
+                          <span
+                            className={state === "collapsed" ? "hidden" : ""}
+                          >
+                            {item.title}
+                          </span>
+                        }
+                      </SidebarMenuButton>
+                    </SidebarMenuItem>
+                  );
+                })}
+              </SidebarMenu>
+            </SidebarGroupContent>
+          </SidebarGroup>
+        ))}
+      </SidebarContent>
+    </>
+  );
 };
 
 export default AppSidebarContent;
--- src/components/layout/AppSidebar/AppSidebarFooter.tsx
+++ src/components/layout/AppSidebar/AppSidebarFooter.tsx
@@ -1,9 +1,22 @@
-import { SidebarFooter } from "@/components/ui/sidebar";
+import {
+  SidebarFooter,
+  SidebarGroup,
+  useSidebar,
+} from "@/components/ui/sidebar";
+import { NavFooterType } from "./nav-data";
 
-const AppSidebarFooter = ({ footer }: { footer: string[] }) => {
+const AppSidebarFooter = ({ footer }: { footer: NavFooterType }) => {
+  const { state } = useSidebar();
   return (
     <SidebarFooter>
-      <h1>App Footer</h1>
+      <SidebarGroup className="items-center text-zinc-500">
+        <h1 className={state === "expanded" ? "text-sm" : "text-xs"}>
+          {footer.title}
+        </h1>
+        <span className={state === "expanded" ? "text-xs" : "hidden"}>
+          {footer.subtitle}
+        </span>
+      </SidebarGroup>
     </SidebarFooter>
   );
 };
