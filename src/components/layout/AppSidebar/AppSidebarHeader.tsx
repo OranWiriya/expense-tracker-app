3f9e3a874a5c9f226e00751f2cd618b99e334f21
OranWiriya <megamanoran@gmail.com>
2026-09-03 16:54:22 +0700

    💄 update type on prop header, using data from prop instead plain text and add cursor pointer to button

--- src/components/layout/AppSidebar/AppSidebarHeader.tsx
+++ src/components/layout/AppSidebar/AppSidebarHeader.tsx
@@ -7,9 +7,9 @@ import {
   SidebarTrigger,
   useSidebar,
 } from "@/components/ui/sidebar";
-import { Cat } from "lucide-react";
+import { NavItemType } from "./nav-data";
 
-const AppSidebarHeader = ({ header }: { header: string[] }) => {
+const AppSidebarHeader = ({ header }: { header: NavItemType }) => {
   const { state } = useSidebar();
   return (
     <SidebarHeader className="border-b">
@@ -17,17 +17,19 @@ const AppSidebarHeader = ({ header }: { header: string[] }) => {
         <SidebarMenuButton
           size={"lg"}
           tooltip={"Expense Tracker"}
-          className="font-bold p-0 pr-2 justify-between group-data-[collapsible=icon]:size-12!  hover:bg-primary hover:text-primary-foreground "
+          className="font-bold p-0 pr-2 justify-between group-data-[collapsible=icon]:size-12! cursor-pointer hover:bg-primary hover:text-primary-foreground "
         >
           <div className="flex size-8 items-center justify-left group-data-[collapsible=icon]:w-8!">
-            <Cat className="size-8! " />
+            {header.icon && <header.icon className="size-8! " />}
           </div>
           {state === "expanded" && (
-            <span className="text-sm">EXPENSE TRACKER</span>
+            <span className="text-sm">{header.title}</span>
           )}
         </SidebarMenuButton>
         <SidebarTrigger
-          className={"size-10 group-data-[collapsible=icon]:size-10!"}
+          className={
+            "size-10 group-data-[collapsible=icon]:size-10! cursor-pointer"
+          }
         />
       </SidebarMenu>
     </SidebarHeader>
